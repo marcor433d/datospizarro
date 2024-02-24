9435c0d5a9395cd433001ad420f1d1194e8de6a0
marcor433d <93809880+marcor433d@users.noreply.github.com>
2024-02-24 14:21:54 -0600

    Add files via upload

---
+++ pizarrogod.sql
@@ -0,0 +1,16 @@
+/*
+-- Query: SELECT * FROM pizarrogod.personas
+LIMIT 0, 1000
+
+-- Date: 2024-02-24 14:12
+*/
+INSERT INTO `` (`edad`,`genero`) VALUES (23,'M');
+INSERT INTO `` (`edad`,`genero`) VALUES (18,'F');
+INSERT INTO `` (`edad`,`genero`) VALUES (35,'O');
+INSERT INTO `` (`edad`,`genero`) VALUES (42,'F');
+INSERT INTO `` (`edad`,`genero`) VALUES (-29,'X');
+INSERT INTO `` (`edad`,`genero`) VALUES (55,'M');
+INSERT INTO `` (`edad`,`genero`) VALUES (-16,'F');
+INSERT INTO `` (`edad`,`genero`) VALUES (78,'M');
+INSERT INTO `` (`edad`,`genero`) VALUES (20,'F');
+INSERT INTO `` (`edad`,`genero`) VALUES (85,'O');
