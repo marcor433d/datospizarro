fd8f8f3694ea004599d1d2791e42595a1b53d410
marcor433d <93809880+marcor433d@users.noreply.github.com>
2024-02-26 13:48:30 -0600

    Add files via upload

---
+++ consultapizarro.sql
@@ -0,0 +1,16 @@
+CREATE TABLE datos (
+    id INTEGER PRIMARY KEY,
+    edad FLOAT,
+    genero VARCHAR(50)
+);
+
+INSERT INTO datos (edad, genero) VALUES
+(-25, 'M'),
+(35, 'F'),
+(85, 'O'),
+(35, 'X'),
+(-42, 'M'),
+(55, 'F'),
+(20, 'M'),
+(28, 'F'),
+(-18, 'O');
